0cfcd2e04341c9b9907c394dcdb86cc46d63743a
Dane Miller <dmille83@cougar.cis.ksu.edu>
2014-10-20 15:37:18 -0500

    Updated simulation.js to be better organized, added functionality to expand/shrink viewport.

--- app/assets/javascripts/simulations.js
+++ app/assets/javascripts/simulations.js
@@ -1,4 +1,6 @@
+// Global variables/settings
 var panViewportToFollowTractor = true;
+var canvasDimensions = [800, 600];
 
 var clampAngle, steerAngle, wrapAngle;
 
@@ -486,7 +488,8 @@ jQuery(function() {
       game.width = simulation.size.width * simulation.size.granularity;
       game.height = simulation.size.height * simulation.size.granularity;
       game.buffers = {
-        front: $("<canvas width=800 height=600>")[0],
+        front: $("<canvas width=" + canvasDimensions[0] + " height=" + canvasDimensions[1] + ">")[0],
+		//front: $("<canvas width=" + game.width + " height=" + game.height + ">")[0],
         back: $("<canvas width=" + game.width + " height=" + game.height + ">")[0],
         terrain: $("<canvas width=" + game.width + " height=" + game.height + ">")[0],
         vegitation: $("<canvas width=" + game.width + " height=" + game.height + ">")[0]
@@ -504,8 +507,8 @@ jQuery(function() {
       game.viewport = {
         x: 0,
         y: 0,
-        width: 800,
-        height: 600,
+        width: canvasDimensions[0],
+        height: canvasDimensions[1],
         radius: 500,
         target: game.hq
       };
@@ -652,6 +655,8 @@ jQuery(function() {
 		if (game.width <= game.viewport.width && game.height <= game.viewport.height)
 		{
 			panmenu.hide();
+			$('#simulation-expand-viewport-button').hide();
+			$('#simulation-shrink-viewport-button').hide();
 		}
 		else
 		{
@@ -1138,18 +1143,24 @@ jQuery(function() {
 	{
 		pausePanTracking();
 	
+		//console.log("x: " + game.viewport.x + " y: " + game.viewport.y);
+	
 		switch (pandirection) {
 			case "up":
 				if (game.viewport.y - movementIncrement >= 0) game.viewport.y = game.viewport.y - movementIncrement;
+				else game.viewport.y = 0;
 				break;
 			case "down":
 				if (game.viewport.y + movementIncrement + game.viewport.height <= game.height) game.viewport.y = game.viewport.y + movementIncrement;
+				else game.viewport.y = game.height - game.viewport.height;
 				break;
 			case "left":
 				if (game.viewport.x - movementIncrement >= 0) game.viewport.x = game.viewport.x - movementIncrement;
+				else game.viewport.x = 0;
 				break;
 			case "right":
 				if (game.viewport.x + movementIncrement + game.viewport.width <= game.width) game.viewport.x = game.viewport.x + movementIncrement;
+				else game.viewport.x = game.width - game.viewport.width;
 				break;
 			default:
 				centerViewportOnTractor();
@@ -1210,6 +1221,25 @@ jQuery(function() {
 	//=============== PAN VIEWPORT FUNCTIONS (end) =================
 	
 	
+	//=============== EXPAND VIEWPORT (begin) =================
+	$('#simulation-expand-viewport-button').on('click', function(event) {
+		panmenu.hide();
+		$('#simulation-expand-viewport-button').hide();
+		$('#simulation-shrink-viewport-button').show();
+		game.viewport.x = 0;
+		game.viewport.y = 0;
+		game.buffers.front.width = game.width;
+		game.buffers.front.height = game.height;
+	});
+	
+	$('#simulation-shrink-viewport-button').on('click', function(event) {
+		panmenu.show();
+		$('#simulation-expand-viewport-button').show();
+		$('#simulation-shrink-viewport-button').hide();
+		game.buffers.front.width = canvasDimensions[0];
+		game.buffers.front.height = canvasDimensions[1];
+	});
+	//=============== EXPAND VIEWPORT (end) =================
 	
 	
     run.on('click', function() {
